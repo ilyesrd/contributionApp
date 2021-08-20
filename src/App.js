35d0a7ca77c654d6b34e6391f70973e9a86fd7c1
ilyesrd <ilyes.rdah@gmail.com>
2021-08-20 21:57:07 +0100

    setup react navigation

---
+++ src/App.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import type {Node} from 'react';
+import Router from './navigation';
+
+const App: () => Node = () => {
+  return <Router />;
+};
+
+export default App;
---
+++ src/navigation/index.js
@@ -0,0 +1,89 @@
+import * as React from 'react';
+import {Button, Text, View} from 'react-native';
+import {NavigationContainer} from '@react-navigation/native';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
+import {createMaterialBottomTabNavigator} from '@react-navigation/material-bottom-tabs';
+import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
+
+function DetailsScreen() {
+  return (
+    <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
+      <Text>Details!</Text>
+    </View>
+  );
+}
+
+function HomeScreen({navigation}) {
+  return (
+    <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
+      <Text>Home screen</Text>
+      <Button
+        title="Go to Details"
+        onPress={() => navigation.navigate('Details')}
+      />
+    </View>
+  );
+}
+
+function SettingsScreen({navigation}) {
+  return (
+    <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
+      <Text>Settings screen</Text>
+      <MaterialCommunityIcons name="bell" size={26} />
+    </View>
+  );
+}
+
+const HomeStack = createNativeStackNavigator();
+const Tab = createMaterialBottomTabNavigator();
+
+function HomeStackScreen() {
+  return (
+    <HomeStack.Navigator>
+      <HomeStack.Screen name="Home" component={HomeScreen} />
+      <HomeStack.Screen name="Details" component={DetailsScreen} />
+    </HomeStack.Navigator>
+  );
+}
+
+export default function Router() {
+  return (
+    <NavigationContainer>
+      <Tab.Navigator
+        initialRouteName="Feed"
+        activeColor="#e91e63"
+        barStyle={{backgroundColor: 'tomato'}}>
+        <Tab.Screen
+          name="Feed"
+          component={HomeStackScreen}
+          options={{
+            tabBarLabel: 'Home',
+            tabBarIcon: ({color}) => (
+              <MaterialCommunityIcons name="home" color={color} size={26} />
+            ),
+          }}
+        />
+        <Tab.Screen
+          name="Notifications"
+          component={SettingsScreen}
+          options={{
+            tabBarLabel: 'Updates',
+            tabBarIcon: ({color}) => (
+              <MaterialCommunityIcons name="bell" color={color} size={26} />
+            ),
+          }}
+        />
+        <Tab.Screen
+          name="Profile"
+          component={SettingsScreen}
+          options={{
+            tabBarLabel: 'Profile',
+            tabBarIcon: ({color}) => (
+              <MaterialCommunityIcons name="account" color={color} size={26} />
+            ),
+          }}
+        />
+      </Tab.Navigator>
+    </NavigationContainer>
+  );
+}
